43a17c812b8bed5589fc40d27177df7c2eaae47b
Ferroudji-Samy <47180338+Ferroudji-Samy@users.noreply.github.com>
2020-04-02 18:07:27 +0200

    ajout fonction choixFmin

--- src/main/java/iia/espacesEtats/algorithmes/AEtoile.java
+++ src/main/java/iia/espacesEtats/algorithmes/AEtoile.java
@@ -5,9 +5,10 @@ package iia.espacesEtats.algorithmes;
 
 import java.util.ArrayList;
 
-import iia.espacesEtats.graphes.Noeud;
+import iia.espacesEtats.graphes.NoeudGF;
 import iia.espacesEtats.modeles.Heuristique;
 import iia.espacesEtats.modeles.Probleme;
+import iia.espacesEtats.modeles.ProblemeACout;
 import iia.espacesEtats.modeles.Solution;
 
 /**
@@ -25,20 +26,31 @@ public class AEtoile implements AlgorithmeHeuristiqueRechercheEE {
     public AEtoile(Heuristique h) {
         this.h = h;
     }
+    
+    private NoeudGF choixFmin(ArrayList<NoeudGF> frontiere) {
+    	NoeudGF min=null;
+    	if(frontiere.size()>0)min=frontiere.get(0);
+    	
+    	for(int i=1;i<frontiere.size()-1;i++) {
+    		if(f(min)>f(frontiere.get(i)))min=frontiere.get(i);
+    	}
+    	return min;
+    }
 
     /* Lance l'exploration sur un problème */
     public Solution chercheSolution(Probleme p) {
         Solution sol = null;
         /* TODO  A compléter/modifier */
-        ArrayList<Noeud> dejaDev= new ArrayList<Noeud>();
-        ArrayList<Noeud> frontiere= new ArrayList<Noeud>();
+        ProblemeACout pcout = (ProblemeACout) p;
+        ArrayList<NoeudGF> dejaDev= new ArrayList<NoeudGF>();
+        ArrayList<NoeudGF> frontiere= new ArrayList<NoeudGF>();
         frontiere.add(sinit);
         /**
          * g(sinit)=0;
          * f(sinit)=h(sinit)
          */
         while(!frontiere.isEmpty()) {
-        	Noeud n=choixFmin(frontiere);
+        	NoeudGF n=choixFmin(frontiere);
         	if(estTerminal(n)) {
         		sol=construireSolution(n,Pere);
         	}
